b6da5d00ebca72927e92c315f079fd046f465bbc
Sean Anderson <sean@seananderson.ca>
2017-05-19 14:25:58 -0700

    Tweak figure

--- figs/plot_hatchery_v_diversity.R
+++ figs/plot_hatchery_v_diversity.R
@@ -3,11 +3,12 @@ library(ggsidekick)
 library(viridis)
 
 d = readRDS("data-generated/hatchery_diversity.rds")
+d$p  <- d$p*0.453592 / 1e6
 
-ggplot(d, aes(p/1000000, tot_div, col=year)) +
-  geom_point(size=2) + xlab("Hatchery harvest (million pounds)") + ylab("Catch diversity") + facet_wrap(~p_fshy, scale="free_x") +
-  scale_color_viridis() + theme_bw() + theme(panel.border = element_blank(), panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) +
-  theme_sleek()
-# +  geom_segment(aes(x=p_1, y=div_1, xend=p, yend=tot_div),
-#    arrow = arrow(length = unit(0.1, "inches")))
+ggplot(d, aes(p, tot_div, col=year)) +
+  geom_point(size=2) +
+  xlab("Hatchery harvest (million kg)") +
+  ylab("Catch diversity") +
+  facet_wrap(~p_fshy, scale="free_x") +
+  scale_color_viridis() + 
+  theme_sleek() + labs(colour = "Year")
