638d2c5ba491cf06a5a182d2c022b8b46097b032
eric-ward <eric.ward@noaa.gov>
2017-05-19 14:01:11 -0700

    update code for plotting

--- analysis/diversity_hatchery_correlation.R
+++ analysis/diversity_hatchery_correlation.R
@@ -50,6 +50,12 @@ s = group_by(cfec, region, year, spec) %>%
   summarize(g = sum(g_earn)) %>%
   group_by(region, year)
 
+# calculate revenue by species-region
+s2 = group_by(cfec, p_fshy, year, spec) %>%
+  summarize(g = sum(g_earn), region=region[1])
+s2 = filter(s2, spec %in% c("COHO", "CHUM", "SOCK", "PINK"))
+s = s2
+
 # merge in the hatchery fraction above
 hatch = d[,c("Year","Region")]
 names(hatch) = tolower(names(hatch))
@@ -75,19 +81,51 @@ simp.div = function(x) {
 s$value[which(is.na(s$value))] = 0
 
 # calculate hatchery and total diversity
-div = group_by(s[which(s$year > 0),], year, region) %>%
+
+div = group_by(s[which(s$year > 0),], year, p_fshy) %>%
   summarize(tot_div = simp.div(g),
     hatch_div = simp.div(g*value),
     wild_div = simp.div(g*(1-value)),
-    p = sum(g*value) / sum(g))
+    p = sum(g*value) / sum(g),
+    p_pink = g[which(spec=="PINK")]*value[which(spec=="PINK")],
+    p_sock = g[which(spec=="SOCK")]*value[which(spec=="PINK")],
+    p_chum = g[which(spec=="CHUM")]*value[which(spec=="CHUM")])
 
-ggplot(div) +
-  facet_wrap(~region) +
-  geom_line(aes(year, hatch_div), color="red") +
-  geom_line(aes(year, wild_div), color="blue") +
-  geom_line(aes(year, tot_div), color="purple") +
-  ylab("Diversity")
+library(viridis)
 
-ggplot(div, aes(year, p)) +
-  facet_wrap(~region) +
-  geom_point(color="red")
+d1 = div[which(div$p_fshy == "S 03E"),]
+d1 = select(d1, year, p_fshy, tot_div, p_sock)
+d1 = rename(d1, p = p_sock)
+
+d2 = div[which(div$p_fshy == "S 01E"),]
+d2 = select(d2, year, p_fshy, tot_div, p_pink)
+d2 = rename(d2, p = p_pink)
+
+d3 = div[which(div$p_fshy == "S 01A"),]
+d3 = select(d3, year, p_fshy, tot_div, p_chum)
+d3 = rename(d3, p = p_chum)
+
+d4 = div[which(div$p_fshy == "S 03A"),]
+d4 = select(d4, year, p_fshy, tot_div, p_chum)
+d4 = rename(d4, p = p_chum)
+
+d = rbind(d1, d2, d3, d4)
+d$species = NA
+d$species[which(d$p_fshy=="S 01A")] = "chum"
+d$species[which(d$p_fshy=="S 01E")] = "pink"
+d$species[which(d$p_fshy=="S 03A")] = "chum"
+d$species[which(d$p_fshy=="S 03E")] = "sockeye"
+
+d$p_fshy[which(d$p_fshy=="S 01A")] = "SE - seine"
+d$p_fshy[which(d$p_fshy=="S 01E")] = "PWS - seine"
+d$p_fshy[which(d$p_fshy=="S 03A")] = "SE - gillnet"
+d$p_fshy[which(d$p_fshy=="S 03E")] = "PWS - gillnet"
+
+# add previous year's data for connecting the dots / arrows
+d$year_1 = d$year - 1
+d$div_1 = d$tot_div[match(paste(d$year_1, d$p_fshy), paste(d$year, d$p_fshy))]
+d$p_1 = d$p[match(paste(d$year_1, d$p_fshy), paste(d$year, d$p_fshy))]
+
+
+
+saveRDS(d, "data-generated/hatchery_diversity.rds")
